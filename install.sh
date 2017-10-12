c1232f3759472e12a45beeb3eac696eb4c37a373
Akansh <akansh.gulati@wingify.com>
2017-10-13 02:35:30 +0530

    feat(install.sh): added options feature for commits

--- install.sh
+++ install.sh
@@ -6,19 +6,17 @@
 # [$2] — (optional) custom semantic message prefix
 function make_git_alias {
   if ! git config --global --get-all alias.$1 &>/dev/null; then
-    if [[ -z $2 ]]; then
-      git config --global alias.$1  '!f() { [[ -z "$GIT_PREFIX" ]] || cd "$GIT_PREFIX" && if [[ -z $1 ]] && [[-z $2]]; then git commit -m "'$1' " -e; else git commit -m "'$1'($1): $2"; fi }; f'
-    else
-      git config --global alias.$1  '!f() { [[ -z "$GIT_PREFIX" ]] || cd "$GIT_PREFIX" && if [[ -z $1 ]]; then git commit -m "'$2': " -e; else git commit -m "'$2': $1"; fi }; f'
-    fi
+    git config --global alias.$1  '!f() { [[ -z "$GIT_PREFIX" ]] || cd "$GIT_PREFIX" && if [[ -z $1 ]] && [[-z $2]]; then git commit -m "'$1' " -e; else git commit -$3m "'$1'($1): $2"; fi }; f'
+    echo 'Alias installed ' $1
+  else
+    echo $1 : 'alias already present, remove it from ~/.gitconfig file first'
   fi
 }
 # Register aliases
-echo 'Installing git aliases…'
+echo 'Installing git aliases'
 
   semantic_aliases=( 'feat' 'fix' 'style' 'cleanup' 'refactor' 'perf' 'test' 'chore' 'tracking' 'docs' )
 
   for semantic_alias in "${semantic_aliases[@]}"; do
-    echo 'Installing git alias '  $semantic_alias
     make_git_alias $semantic_alias
   done
